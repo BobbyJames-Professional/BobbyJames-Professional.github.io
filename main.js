7028f7889f1d62b21ef14ce64ea8829f5c66c934
Horan <108380@GLTHS.GLTECH.ORG>
2026-04-10 12:38:20 -0400

    The Certification pdfs were not working, changing path in an attempt to fix

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 //This may work outside of iFrames but could lead to bad displays
 function toggleVisibleIFrame(elementID) {
     var element = document.getElementById(elementID);
-    if (element.style.display == 'none') {
-        element.style.display = 'block';
-    } else {
+    if (element.style.display == 'block') {
         element.style.display = 'none';
+    } else {
+        element.style.display = 'block';
     }
 }
